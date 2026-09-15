4b5791126018fab7a2f21eae1ee1e72e88b5f5ab
UCIRANX <uciranx@gmail.com>
2026-09-16 00:39:30 +0330

    MmkvManager.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/handler/MmkvManager.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/handler/MmkvManager.kt
@@ -677,7 +677,7 @@ object MmkvManager {
      * @param guid The subscription GUID.
      * @param subItem The subscription item.
      */
-    fun encodeSubscription(guid: String, subItem: SubscriptionItem) {
+    fun encodeSubscription(guid: String, subItem: SubscriptionItem): String {
         val key = guid.ifBlank { Utils.getUuid() }
         subStorage.encode(key, JsonUtil.toJson(subItem))
 
@@ -686,6 +686,7 @@ object MmkvManager {
             subsList.add(key)
             encodeSubsList(subsList)
         }
+        return key
     }
 
     /**
