9458bafa770d297950e329291613657a70375fc1
UCIRANX <uciranx@gmail.com>
2026-09-16 11:11:07 +0330

    MainScreen.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/main/MainScreen.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/main/MainScreen.kt
@@ -3,6 +3,7 @@ package com.v2ray.ang.ui.main
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.grid.LazyGridState
